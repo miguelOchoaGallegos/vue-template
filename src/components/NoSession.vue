989423e556e8ba449a3912e24730cbc53e49e303
miguel.ochoa <miguel.ochoa.gallegos@gmail.com>
2018-05-17 16:20:33 -0500

    cambio descripcion

--- src/components/NoSession.vue
+++ src/components/NoSession.vue
@@ -5,7 +5,7 @@
     <b-col md="6">
       <div class="jumbotron">
       <h1>No autorizado</h1>
-      <p class="lead">Usted no se encuentra autorizado para visualizar el contenido.</p>
+      <p class="lead">Ud. no se encuentra autorizado para visualizar el contenido.</p>
       <!-- <p><router-link to="/">volver al login</router-link></p> -->
       <p>      
       <b-button variant="danger" href="/">volver al login</b-button>
